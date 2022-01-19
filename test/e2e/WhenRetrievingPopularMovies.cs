c6bbf916386a9579401d30f342a2282fa35b86b1
Daniel Robinson <521946+DanielRobinsonSoftware@users.noreply.github.com>
2022-01-19 21:58:28 +1300

    Add authentication to end-to-end test setup

--- test/e2e/WhenRetrievingPopularMovies.cs
+++ test/e2e/WhenRetrievingPopularMovies.cs
@@ -1,25 +1,63 @@
 using System;
 using Xunit;
+using Xunit.Abstractions;
 using RestSharp;
 using FluentAssertions;
+using Newtonsoft.Json;
 
 namespace e2e
 {
     public class WhenRetrievingPopularMovies
     {
-        // TODO: Given I have authenticated
+        private RestClient _apiClient;
+        private string _bearerToken;
+        private readonly ITestOutputHelper _output;
+
+        public WhenRetrievingPopularMovies(ITestOutputHelper output)
+        {
+            _output = output;
+
+            var apiAppName = Environment.GetEnvironmentVariable("API_APP_NAME");
+            _apiClient = new RestClient($"https://{apiAppName}-staging.azurewebsites.net/api/v1/");
+
+            GivenIHaveAuthenticated();
+        }
+
+        private void GivenIHaveAuthenticated()
+        {
+            // https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow
+            var instance = Environment.GetEnvironmentVariable("AZURE_AD_INSTANCE");
+            var tenantId = Environment.GetEnvironmentVariable("AZURE_AD_TENANT_ID");
+            var tokenClient = new RestClient($"{instance}{tenantId}/oauth2/v2.0/token");
+            var request = new RestRequest();
+            
+            request.AddParameter("client_id", Environment.GetEnvironmentVariable("WEB_APP_CLIENT_ID")); // The web app's application ID.
+            request.AddParameter("scope", Environment.GetEnvironmentVariable("API_SCOPE")); // The resource identifier (application ID URI) of the resource you want, affixed with the .default suffix. 
+            request.AddParameter("client_secret", Environment.GetEnvironmentVariable("WEB_APP_CLIENT_SECRET")); // The client secret generated for the app in the app registration portal.
+            request.AddParameter("grant_type", "client_credentials"); // Must be set to client_credentials.
+
+            var response = tokenClient.Post(request);
+
+            if (!response.IsSuccessful)
+            {
+                throw new Exception("Could not obtain authentication token");
+            }
+
+            _bearerToken = JsonConvert.DeserializeObject<TokenResponse>(response.Content).AccessToken;
+        }
 
         [Fact]
         public void ThenResponseIsSuccessful()
         {
-            // TODO: read app name from env variable
-            var client = new RestClient("https://moviematch211027-staging.azurewebsites.net/api/v1/");
-
             var request = new RestRequest("movies/popular");
+            request.AddHeader("Authorization", $"Bearer {_bearerToken}");
 
-            var response = client.Get(request);
+            // Act
+            var response = _apiClient.Get(request);
 
-            response.IsSuccessful.Should().BeTrue();
+            // Assert
+            _output.WriteLine($"Reponse status is: {response.StatusCode} {response.StatusDescription}");
+            response.IsSuccessful.Should().BeTrue();            
         }
 
         // TODO: Tests to validate the expected structure of the response content
